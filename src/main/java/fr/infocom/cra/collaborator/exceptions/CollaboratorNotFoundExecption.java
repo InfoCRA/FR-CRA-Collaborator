b30bc6b231e1bbfefd04be072e388e59ad317fb0
INFOCOMGROUP\ibenhsine <benhsine.ilham@gmail.com>
2021-05-05 17:06:51 +0200

    Controller Collab and excepetion

---
+++ src/main/java/fr/infocom/cra/collaborator/exceptions/CollaboratorNotFoundExecption.java
@@ -0,0 +1,8 @@
+package fr.infocom.cra.collaborator.exceptions;
+
+public class CollaboratorNotFoundExecption extends RuntimeException {
+
+	public CollaboratorNotFoundExecption(String string) {
+		super(string);
+	}
+}
