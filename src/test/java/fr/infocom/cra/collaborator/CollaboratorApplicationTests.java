79d4f4b9fd1da9439ef85817867b43931e1cd656
INFOCOMGROUP\ibenhsine <benhsine.ilham@gmail.com>
2021-05-05 15:52:00 +0200

    inial project

---
+++ src/test/java/fr/infocom/cra/collaborator/CollaboratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.infocom.cra.collaborator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
