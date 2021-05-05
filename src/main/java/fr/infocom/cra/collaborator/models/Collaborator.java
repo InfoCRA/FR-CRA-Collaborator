50a5c7af06316b8b90baa99fb5ab2825b4e89951
INFOCOMGROUP\ibenhsine <benhsine.ilham@gmail.com>
2021-05-05 16:50:22 +0200

    Models added

---
+++ src/main/java/fr/infocom/cra/collaborator/models/Collaborator.java
@@ -0,0 +1,27 @@
+package fr.infocom.cra.collaborator.models;
+
+/**
+ * <b>Collaborator is a representation of a staff member of Infocom Group.</b>
+ * <p>
+ * A collaborator is identified with this informations :
+ * <ul>
+ * <li>A unique identificator id.</li>
+ * <li>A last name</li>
+ * <li>A first name</li>
+ * <li>An email address</li>
+ * <li>A password</li>
+ * <li>A date of entry</li>
+ * <li>A date of release</li>
+ * <li>A type, internal, external, commercial, etc.</li>
+ * </ul>
+ * </p>
+ * 
+ * @see 
+ * 
+ * @author ibenhsine
+ * @version 1.0
+ */
+
+public class Collaborator {
+
+}
